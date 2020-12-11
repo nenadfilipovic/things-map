d149e1e95e1b487ce41616a300dc7442693ec83f
nenadfilipovic <nenadfilipovic@tutanota.com>
2020-12-11 11:54:08 +0000

    Configure GraphQL linting and add basic schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,4 @@
-import fs from 'fs';
 import path from 'path';
-import { print } from 'graphql';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
 
@@ -9,8 +7,5 @@ const resolversArray = loadFilesSync(path.join(__dirname, '**/resolvers.ts'));
 
 const typeDefs = mergeTypeDefs(typesArray);
 const resolvers = mergeResolvers(resolversArray);
-
-const printedTypeDefs = print(typeDefs);
-fs.writeFileSync('schema.graphql', printedTypeDefs);
 
 export { typeDefs, resolvers };
