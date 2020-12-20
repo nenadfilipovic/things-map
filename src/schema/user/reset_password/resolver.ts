a6e8b22f407fb57ac777a372b66cb88e22f1eb74
nenadfilipovic <nenadfilipovic@tutanota.com>
2020-12-20 10:34:47 +0000

    Separate forgot and reset password

--- src/schema/user/reset_password/resolver.ts
+++ src/schema/user/reset_password/resolver.ts
@@ -1,101 +1,14 @@
 import { raw } from 'objection';
+import { config } from 'src/config';
 import { mail } from 'src/services/mail';
-import { formatDate } from 'src/services/date';
 import { User } from 'src/database/models/User';
-import { generateCode } from 'src/services/codegen';
-import { clearToken } from 'src/authentication/jwt';
-import { hashPassword } from 'src/authentication/password';
-import { config, resetPasswordTokenMaxAge } from 'src/config';
-import {
-  ForgotPasswordResult,
-  ResetPasswordResult,
-  Resolvers,
-} from 'src/generated/resolverTypes';
+import { hashPassword } from 'src/services/password';
+import { generateRandomToken } from 'src/services/generator';
+import { clearAuthenticationToken } from 'src/services/authentication';
+import { ResetPasswordResult, Resolvers } from 'src/generated/resolverTypes';
 
 const resolvers: Resolvers = {
   Mutation: {
-    /**
-     *
-     * @param root
-     * @param param1
-     * @param context
-     *
-     * Initiate forgot password process.
-     */
-
-    forgotPassword: async (
-      root,
-      { input },
-      context,
-    ): Promise<ForgotPasswordResult> => {
-      /**
-       * Prepare data.
-       */
-
-      const { email } = input;
-      const user = await User.query().findOne({ email, isVerified: true });
-
-      /**
-       * Send same response even if user doesn't exist.
-       */
-
-      if (user) {
-        /**
-         * Prepare data.
-         */
-
-        const {
-          id,
-          email,
-          username,
-          resetPasswordToken,
-          resetPasswordTokenExpires,
-        } = user;
-
-        let token = resetPasswordToken;
-        let tokenExpires = resetPasswordTokenExpires;
-
-        /**
-         * Don't create new token if existing one is valid.
-         */
-
-        if (token === null || tokenExpires < formatDate(Date.now())) {
-          /**
-           * Generate new token and set its life to 30 minutes.
-           */
-
-          token = generateCode(16);
-          tokenExpires = formatDate(Date.now() + resetPasswordTokenMaxAge);
-
-          /**
-           * Update user data.
-           */
-
-          await User.query()
-            .patch({
-              resetPasswordToken: token,
-              resetPasswordTokenExpires: tokenExpires,
-            })
-            .findById(id);
-        }
-
-        /**
-         * Send email with token.
-         */
-
-        mail.sendMail({
-          from: config.EMAIL_USERNAME,
-          to: email,
-          subject: `Forgot password request from user: ${username}`,
-          text: `Please use this token to reset your password. \n Token: ${token} \n Token is valid until: ${tokenExpires}`,
-        });
-      }
-
-      return {
-        message: `Thanks! If there's an account associated with this email, we'll send the password reset instructions immediately`,
-      };
-    },
-
     /**
      *
      * @param root
@@ -121,7 +34,7 @@ const resolvers: Resolvers = {
        * If token doesn't exist or is invalid return message.
        */
 
-      if (user?.resetPasswordTokenExpires > formatDate(Date.now())) {
+      if (user?.resetPasswordTokenExpires > new Date()) {
         /**
          * Prepare data.
          */
@@ -132,14 +45,14 @@ const resolvers: Resolvers = {
          * Reset helper variables, create new password and update user data.
          */
 
-        const password = generateCode(24);
+        const password = generateRandomToken(24);
 
         await User.query()
           .patch({
-            password: await hashPassword(password),
             resetPasswordToken: raw('NULL'),
+            lastPasswordChangedDate: new Date(),
             resetPasswordTokenExpires: raw('NULL'),
-            lastPasswordChangedDate: formatDate(Date.now()),
+            password: await hashPassword(password),
           })
           .findById(id);
 
@@ -155,10 +68,10 @@ const resolvers: Resolvers = {
         });
 
         /**
-         * Remove auth cookies.
+         * Deauthenticate user.
          */
 
-        clearToken(ctx);
+        clearAuthenticationToken(ctx);
 
         return {
           message: 'Please check your email address for your new password',
